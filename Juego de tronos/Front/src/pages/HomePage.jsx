b77f56ffd8d2497f83eee54dbb9dae7f86343a58
Serberar <davorsg@gmail.com>
2023-04-22 11:41:52 +0200

    traductor

--- Juego de tronos/Front/src/pages/HomePage.jsx
+++ Juego de tronos/Front/src/pages/HomePage.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import '../css/Home.css';
+import { useTranslation } from 'react-i18next';
+import i18n from '../translate/i18n';
 
+function LanguageSwitcher() {
+  const { t } = useTranslation();
+
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
+}
 const HomePage = () => {
   return (
     <div className='home-div'>
+     <div>
+      <button onClick={() => changeLanguage('en')}>{t('English')}</button>
+      <button onClick={() => changeLanguage('es')}>{t('Español')}</button>
+    </div>
     <div className='banderas'>
         <img className='idioma' src="./img/ingles.jpg" alt='English'/>
         <img className='idioma' src="./img/castellano.svg" alt='Español'/>
